f668cdd6bae98a1def734915666642663e8f3c32
asrinandirin <asrnandrn@gmail.com>
2023-10-13 12:20:13 +0300

    first-commit

---
+++ main.tf
@@ -0,0 +1,57 @@
+locals {
+  zone = var.instances_zone
+  instances = [for name in var.instances : "zones/${local.zone}/instances/${name}"]
+  prefixes = var.instance_prefix
+}
+
+resource "google_os_config_patch_deployment" "sandbox-deployment" {
+
+  project = var.project
+  patch_deployment_id = var.patch_deployment_id
+
+   instance_filter {
+        instances = local.instances
+        instance_name_prefixes = local.prefixes
+    }
+
+    patch_config {
+        
+        reboot_config = var.reboot_config
+
+        yum {
+        minimal = var.minimal
+        security = var.security
+        excludes = var.excludes
+        exclusive_packages = var.exclusive_packages
+        }
+
+        windows_update {
+          classifications = var.classifications
+        }
+
+    }
+  
+    duration = var.duration
+
+    recurring_schedule {
+
+        time_zone {
+        id = var.time_zone
+        }
+
+        time_of_day {
+        hours = var.time_of_day[0]
+        minutes = var.time_of_day[1]
+        seconds = var.time_of_day[2]
+        nanos = var.time_of_day[3]
+        }
+
+        monthly {
+        month_day = 2
+        }
+    }
+
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,74 @@
+variable "patch_deployment_id" {
+    type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "instances" {
+  type = list(string)
+}
+
+variable "instances_zone" {
+  type = string
+}
+
+variable "instance_prefix" {
+  type = list(string)
+  default = []
+}
+
+// YUM CONFIGS
+
+variable "security" {
+  type = bool
+}
+
+variable "minimal" {
+  type = bool
+  default = false
+}
+
+variable "excludes" {
+  type = list(string)
+  default = []
+}
+
+variable "exclusive_packages" {
+  type = list(string)
+  default = []
+}
+
+// WINDOWS CONFIGS 
+
+variable "classifications" {
+  type = list(string)
+  default = []
+}
+
+variable "duration" {
+  type = string
+  default = "5600s"
+}
+
+variable "reboot_config" {
+  type = string
+  default = "NEVER"
+}
+
+variable "time_zone" {
+  type = string
+  default = "Europe/Istanbul"
+}
+
+// Time, Default 12.30 AM
+
+variable "time_of_day" {
+  type = list(number)
+  default = [ 0,30,0,0 ]
+}
+
+
+
+
